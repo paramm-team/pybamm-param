3ea49ed4f8e9929785a94bf90370389740718f5a
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2022-07-31 16:14:15 +0100

    add dev install

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
+import os
+import site
+import shutil
 
 install_requires = [
     "pybamm",
@@ -14,4 +17,17 @@ setup(
     license="LICENSE",
     description="A package for optimising parameters in PyBaMM. Under development.",
     install_requires=install_requires,
+    extras_require={
+        "docs": ["sphinx>=1.5", "guzzle-sphinx-theme"],  # For doc generation
+        "dev": [
+            "flake8>=3",  # For code style checking
+            "black",  # For code style auto-formatting
+        ],
+    },
 )
+
+# pybtex adds a folder "tests" to the site packages, so we manually remove this
+path_to_sitepackages = site.getsitepackages()[0]
+path_to_tests_dir = os.path.join(path_to_sitepackages, "tests")
+if os.path.exists(path_to_tests_dir):
+    shutil.rmtree(path_to_tests_dir)
