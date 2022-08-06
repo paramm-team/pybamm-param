47e911329c81037117c3a767106e32a44607e273
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2022-08-06 19:19:11 +0100

    update solver name

--- tests/unit/test_optimisers/test_scipy_differential_evolution.py
+++ tests/unit/test_optimisers/test_scipy_differential_evolution.py
@@ -5,8 +5,6 @@ import pbparam
 
 import unittest
 
-from tests.shared import DummyOptimisationProblem
-
 
 class TestScipyDifferentialEvolution(unittest.TestCase):
     def test_scipy_differential_evolution_init(self):
@@ -17,7 +15,7 @@ class TestScipyDifferentialEvolution(unittest.TestCase):
         self.assertEqual(optimiser.extra_options, {})
 
     def test_optimiser(self):
-        opt = DummyOptimisationProblem()
+        opt = pbparam.BaseOptimisationProblem()
         opt.cost_function = lambda x: x[0] ** 2
         opt.x0 = 2
         opt.bounds = [[-10, 10]]
@@ -27,7 +25,7 @@ class TestScipyDifferentialEvolution(unittest.TestCase):
         self.assertAlmostEqual(result.x[0], 0, places=6)
 
     def test_optimiser_multiple_workers(self):
-        opt = DummyOptimisationProblem()
+        opt = pbparam.BaseOptimisationProblem()
         opt.cost_function = lambda x: x[0] ** 2
         opt.x0 = 2
         opt.bounds = [[-10, 10]]
