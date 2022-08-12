c60b14a43bec84e5350a8a5c1108310bc9454574
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2022-08-12 11:47:51 +0100

    setup hatch

--- pbparam/__init__.py
+++ pbparam/__init__.py
@@ -1,4 +1,9 @@
 #
+# Version info
+#
+from pbparam.version import __version__
+
+#
 # Optimisers
 #
 from .optimisers.base_optimiser import BaseOptimiser
@@ -14,3 +19,5 @@ from .optimisation_problem import OptimisationProblem
 # Optimisation result
 #
 from .optimisation_result import OptimisationResult
+
+__version__ = 0.1
