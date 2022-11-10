e4d7fa7a2991ff31c3833de302a45fbef779f385
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2022-11-10 18:19:25 +0000

    #12 add test ocp_balance

--- tests/unit/test_optimisation_problems/test_OCP_balance.py
+++ tests/unit/test_optimisation_problems/test_OCP_balance.py
@@ -2,8 +2,7 @@
 # Tests for the Data Fit class
 #
 import pbparam
-# import numpy as np
-# import pandas as pd
+import pandas as pd
 
 import unittest
 
@@ -18,44 +17,43 @@ class TestOCPBalance(unittest.TestCase):
         self.assertEqual(optimisation_problem.data_fit, ["data_fit"])
         self.assertEqual(optimisation_problem.data_ref, ["data_ref"])
 
-    # def test_OCP_balance_data(self):
-    #     data_fit = pd.DataFrame(
-    #         {
-    #             0: [7, 5, 3, 1, 3, 5, 7],
-    #             1: [3.9, 2.9, 1.9, 1, 2, 3, 4] + np.random.normal(0, 1e-2, 7),
-    #         }
-    #     )
-    #     data_ref = (
-    #         pd.DataFrame({0: [0, 1, 2, 3], 1: [3.9, 2.9, 1.9, 1]}),
-    #         pd.DataFrame({0: [3, 2, 1, 0], 1: [1, 2, 3, 4]}),
-    #     )
-    #     optimisation_problem = pbparam.OCPBalance(data_fit, data_ref)
+    def test_OCP_balance(self):
+        data_ref = [
+            pd.DataFrame({0: [0.1, 0.3, 0.5, 0.7, 0.9], 1: [5, 4, 3, 2, 1]}),
+            pd.DataFrame({0: [0.1, 0.3, 0.5, 0.7, 0.9], 1: [6, 5, 4, 3, 2]}),
+        ]
 
-    #     optimisation_problem.setup_cost_function()
+        # Test decreasing fit data
+        data_fit = [
+            pd.DataFrame({0: [1, 2, 3, 4, 5], 1: [5, 4, 3, 2, 1]}),
+            pd.DataFrame({0: [1, 2, 3, 4, 5], 1: [6, 5, 4, 3, 2]}),
+        ]
+        optimisation_problem = pbparam.OCPBalance(data_fit, data_ref)
 
-    #     np.testing.assert_array_equal(
-    #         optimisation_problem.data_fit_ch.to_numpy(),
-    #         np.array([[1, 3, 5, 7], [1.1, 1.9, 3.1, 3.9]]).transpose(),
-    #     )
-    #     np.testing.assert_array_equal(
-    #         optimisation_problem.data_fit_dch.to_numpy(),
-    #         np.array([[7, 5, 3, 1], [3.9, 3, 1.8, 1]]).transpose(),
-    #     )
-    #     np.testing.assert_array_equal(
-    #         optimisation_problem.data_ref_ch([0, 1, 2, 3]),
-    #         np.array([0.9, 1.9, 2.9, 4]),
-    #     )
-    #     np.testing.assert_array_equal(
-    #         optimisation_problem.data_ref_dch([0, 1, 2, 3]), np.array([1, 2, 3, 4])
-    #     )
+        optimisation_problem.setup_cost_function()
 
-    #     self.assertEqual(optimisation_problem.x0, [7 / 6, -1 / 6])
-    #     self.assertEqual(optimisation_problem.bounds, [(-0.1, 1.1), (-1.1 / 7, 0.1)])
+        # Check bounds are correct
+        self.assertEqual(optimisation_problem.x0, [-0.25, 0.25])
+        self.assertEqual(optimisation_problem.bounds, [(-0.275, 1.1), (-0.1, 0.275)])
 
-    #     # TODO: fix once the initial guess is improved
-    #     self.assertAlmostEqual(
-    #         optimisation_problem.cost_function([-0.5, 0.5]), 0.016347239
-    #     )
+        # Check cost function is zero at theoretical optimal
+        self.assertAlmostEqual(optimisation_problem.cost_function([-0.1, 0.2]), 0)
+
+        # Test increasing fit data
+        data_fit = [
+            pd.DataFrame({0: [1, 2, 3, 4, 5], 1: [1, 2, 3, 4, 5]}),
+            pd.DataFrame({0: [1, 2, 3, 4, 5], 1: [2, 3, 4, 5, 6]}),
+        ]
+        optimisation_problem = pbparam.OCPBalance(data_fit, data_ref)
+
+        optimisation_problem.setup_cost_function()
+
+        # Check bounds are correct
+        self.assertEqual(optimisation_problem.x0, [1.25, -0.25])
+        self.assertEqual(optimisation_problem.bounds, [(-0.1, 1.375), (-0.275, 0.1)])
+
+        # Check cost function is zero at theoretical optimal
+        self.assertAlmostEqual(optimisation_problem.cost_function([1.1, -0.2]), 0)
 
 
 if __name__ == "__main__":
