ae4004a38e1c6e42b41ce60c0eab90b276d47334
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2022-11-10 16:29:28 +0000

    #12 clean example

--- examples/scripts/ocp_balance.py
+++ examples/scripts/ocp_balance.py
@@ -11,7 +11,6 @@ anode_three_delit = pd.read_csv(os.path.join("data", "anode_OCP_3_delit.csv"))
 ocp_balance = pbparam.OCPBalance(
     [anode_three_lit, anode_three_delit], [anode_half_lit, anode_half_delit]
 )
-ocp_balance.setup_cost_function()
 optimiser = pbparam.ScipyMinimize(
     method="Nelder-Mead", solver_options={"xatol": 1e-12, "fatol": 1e-12}
 )
@@ -29,7 +28,6 @@ cathode_three_delit = pd.read_csv(os.path.join("data", "cathode_OCP_3_delit.csv"
 ocp_balance = pbparam.OCPBalance(
     [cathode_three_lit, cathode_three_delit], [cathode_half_lit, cathode_half_delit]
 )
-ocp_balance.setup_cost_function()
 optimiser = pbparam.ScipyMinimize(method="Nelder-Mead", extra_options={"tol": 1e-6})
 
 result = optimiser.optimise(ocp_balance)
