43e65633dfba33f8d9275fc828fc36511a25c0f7
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2023-07-25 15:34:59 +0100

    move to models folder

--- pbparam/__init__.py
+++ pbparam/__init__.py
@@ -23,7 +23,7 @@ from .optimisers.scipy_differential_evolution import ScipyDifferentialEvolution
 from .optimisation_problems.base_optimisation_problem import BaseOptimisationProblem
 from .optimisation_problems.data_fit import DataFit
 from .optimisation_problems.OCP_balance import OCPBalance
-from .optimisation_problems.basic_gitt import BasicGITT
+from .models.basic_gitt import BasicGITT
 
 #
 # Optimisation result
