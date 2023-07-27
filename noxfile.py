15ec88391185351402b7067e4e501e75d042f020
PipGrylls <pipgryllsastro@gmail.com>
2023-07-27 09:41:41 +0100

    nox sphinx completes

--- noxfile.py
+++ noxfile.py
@@ -3,12 +3,14 @@ import nox
 
 @nox.session
 def lint(session):
+    """Run the flake8 linter."""
     session.install('flake8')
     session.run('flake8', './pbparam/', './tests/')
 
 
 @nox.session
 def sphinx(session):
+    """Build the documentation."""
     session.install('-e', './[docs]')
     session.run('sphinx-build', '-b', 'html', 'docs', 'docs/_build/html', '-W')
 
@@ -17,12 +19,12 @@ def sphinx(session):
 def integration(session):
     """Run the intergration test suite."""
     session.install('-e', './[dev]')
-    session.run()
+    session.run("./tests/integration/")
 
 
 @nox.session(python=['3.8', '3.9', '3.10', '3.11'])
 def unit(session):
     """Run the unit test suite."""
     session.install('-e', './[dev]')
-    session.run()
+    session.run("./tests/unit/")
 
