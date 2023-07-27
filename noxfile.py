de42e88bc96535bf48b9b730545db24ff09de121
PipGrylls <pipgryllsastro@gmail.com>
2023-07-27 10:04:51 +0100

    nox unit completes

--- noxfile.py
+++ noxfile.py
@@ -19,12 +19,26 @@ def sphinx(session):
 def integration(session):
     """Run the intergration test suite."""
     session.install('-e', './[dev]')
-    session.run("./tests/integration/")
+
+    # Get the files to test
+    if session.posargs:
+        test_files = session.posargs
+    else:
+        test_files = ["./tests/integration/"]
+
+    session.run("python", "-m", "unittest", "discover", *test_files)
 
 
 @nox.session(python=['3.8', '3.9', '3.10', '3.11'])
 def unit(session):
     """Run the unit test suite."""
     session.install('-e', './[dev]')
-    session.run("./tests/unit/")
+
+    # Get the files to test
+    if session.posargs:
+        test_files = session.posargs
+    else:
+        test_files = ["./tests/unit/"]
+
+    session.run("python", "-m", "unittest", "discover", *test_files)
 
